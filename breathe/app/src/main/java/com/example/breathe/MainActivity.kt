e62cc8f6f2b8d395b91b496bf5e29b30cf9735f3
Astana-Mirza <lyulin.2003@mail.ru>
2024-02-25 20:23:40 +0300

    add buttons to cards in main activity

--- breathe/app/src/main/java/com/example/breathe/MainActivity.kt
+++ breathe/app/src/main/java/com/example/breathe/MainActivity.kt
@@ -3,13 +3,28 @@ package com.example.breathe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.integerArrayResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.breathe.ui.theme.BreatheTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +32,88 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             BreatheTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    PracticeCard("Android")
-                }
+                CardsLayout(Modifier.fillMaxSize())
             }
         }
     }
 }
 
 @Composable
-fun PracticeCard(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun PracticeCard(practiceNum: Int, modifier: Modifier = Modifier) {
+    OutlinedCard(
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ),
+        modifier = modifier.padding(10.dp, 30.dp, 10.dp, 0.dp)
+    ) {
+        Text(
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center,
+            text = stringArrayResource(R.array.exercise_name)[practiceNum],
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+                .padding(0.dp, 3.dp, 0.dp, 5.dp)
+        )
+        Text(
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodySmall,
+            text = stringArrayResource(R.array.exercise_desc)[practiceNum],
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(15.dp, 10.dp, 15.dp, 5.dp)
+        )
+        Text(
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodySmall,
+            text = integerArrayResource(R.array.exercise_time)[practiceNum].toString()
+                + " " + stringResource(R.string.min),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(15.dp, 10.dp, 15.dp, 5.dp)
+        )
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall,
+                text = stringResource(R.string.select),
+            )
+        }
+    }
+}
+
+@Composable
+fun CardsLayout(modifier: Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        val numbers = (0..3).toList()
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            verticalArrangement = Arrangement.Top,
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            userScrollEnabled = true,
+            modifier = modifier
+        ) {
+            items(numbers.size) {
+                PracticeCard(
+                    practiceNum = it,
+                    modifier = modifier
+                )
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun PracticeCardPreview() {
+fun CardsLayoutPreview() {
     BreatheTheme {
-        PracticeCard("Android")
+        CardsLayout(Modifier.fillMaxSize())
     }
 }
--- breathe/app/src/main/java/com/example/breathe/ui/theme/Type.kt
+++ breathe/app/src/main/java/com/example/breathe/ui/theme/Type.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -14,21 +13,26 @@ val Typography = Typography(
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
+    ),
+    bodySmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontSize = 12.sp,
+        lineHeight = 15.sp,
+        letterSpacing = 0.5.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
+    titleSmall = TextStyle(
         fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.3.sp
     )
-    */
 )
