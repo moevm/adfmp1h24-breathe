7567d87f3e3b9905ba39c73aaf7d974d4933ded2
Alekseev Roman <alexeevroma2002@mail.ru>
2024-03-03 20:40:28 +0300

    add background circles and timer

--- breathe/app/src/main/java/com/example/breathe/TrainingActivity.kt
+++ breathe/app/src/main/java/com/example/breathe/TrainingActivity.kt
@@ -3,29 +3,36 @@ package com.example.breathe
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Info
 import androidx.compose.material3.Divider
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.integerArrayResource
 import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -57,10 +64,87 @@ fun TrainingLayout(practiceNum: Int, modifier: Modifier) {
                 modifier = Modifier
             ) {
                 TrainingHeader(practiceNum)
+                TrainingTimer()
             }
             TrainingStopButton()
         }
     }
+}
+
+@Composable
+fun TrainingTimer(modifier: Modifier = Modifier) {
+    var minutes by remember { mutableStateOf(0) }
+    var seconds by remember { mutableStateOf(0) }
+
+    Box (
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+    ) {
+        CircleBackground(
+            integerArrayResource(R.array.background_circle_radius)[0],
+            colorResource(R.color.timer_background_light),
+            0.15F
+        )
+        CircleBackground(integerArrayResource(
+            R.array.background_circle_radius)[1],
+            colorResource(R.color.timer_background_light),
+            0.3F
+        )
+        CircleBackground(integerArrayResource(
+            R.array.background_circle_radius)[2],
+            colorResource(R.color.timer_background_light),
+            0.5F
+        )
+        Column (
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text (
+                    text = minutes.toString(),
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 80.sp,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(
+                    text = stringResource(R.string.min),
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 50.sp,
+                    modifier = Modifier
+                )
+            }
+            Row (
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text (
+                    text = seconds.toString(),
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 80.sp,
+                    modifier = Modifier
+                )
+                Spacer(modifier = Modifier.size(10.dp))
+                Text(
+                    text = stringResource(R.string.sec),
+                    color = MaterialTheme.colorScheme.secondary,
+                    fontSize = 50.sp,
+                    modifier = Modifier
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CircleBackground(radius: Int, color: Color, alpha: Float, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .size(radius.dp)
+            .clip(CircleShape)
+            .background(color)
+            .alpha(alpha)
+    )
 }
 
 @Composable
