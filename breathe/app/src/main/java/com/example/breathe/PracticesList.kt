dad184b5cf119440e533b09ad305614a70c6f5bf
Astana-Mirza <lyulin.2003@mail.ru>
2024-03-05 20:38:13 +0300

    add navhost

---
+++ breathe/app/src/main/java/com/example/breathe/PracticesList.kt
@@ -0,0 +1,167 @@
+package com.example.breathe
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedCard
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.integerArrayResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringArrayResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.breathe.ui.theme.BreatheTheme
+
+@Composable
+fun PracticeCard(practiceNum: Int, modifier: Modifier = Modifier) {
+    OutlinedCard(
+        border = BorderStroke(1.dp, MaterialTheme.colorScheme.outline),
+        colors = CardDefaults.cardColors(
+            containerColor = MaterialTheme.colorScheme.background,
+            contentColor = MaterialTheme.colorScheme.secondary
+        ),
+        modifier = modifier.padding(10.dp, 30.dp, 10.dp, 0.dp)
+    ) {
+        Text(
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleSmall,
+            textAlign = TextAlign.Center,
+            text = stringArrayResource(R.array.exercise_name)[practiceNum],
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+                .padding(0.dp, 3.dp, 0.dp, 5.dp)
+        )
+        Text(
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.bodySmall,
+            text = stringArrayResource(R.array.exercise_desc)[practiceNum],
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(15.dp, 10.dp, 15.dp, 5.dp)
+        )
+        Row()
+        {
+            Icon(
+                painter = painterResource(R.drawable.clock),
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.secondary,
+                modifier = Modifier
+                    .padding(15.dp, 10.dp, 0.dp, 10.dp)
+                    .scale(0.8F)
+            )
+            Text(
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.bodySmall,
+                text = integerArrayResource(R.array.exercise_time)[practiceNum].toString()
+                        + " " + stringResource(R.string.min),
+                modifier = Modifier
+                    .padding(5.dp, 10.dp, 15.dp, 10.dp)
+                    .align(Alignment.CenterVertically)
+            )
+        }
+        FooterButton(stringResource(R.string.select)) {
+            /*TODO*/
+        }
+    }
+}
+
+@Composable
+fun MainHeader(modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(0.dp, 10.dp, 0.dp, 0.dp)
+            .verticalScroll(rememberScrollState())
+    ) {
+        IconButton(
+            modifier = modifier.scale(1.6F),
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                Icons.Outlined.Settings,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.secondary,
+                modifier = modifier
+            )
+        }
+        Text(
+            text = stringResource(R.string.app_name),
+            color = MaterialTheme.colorScheme.secondary,
+            style = MaterialTheme.typography.titleLarge,
+            modifier = modifier.align(Alignment.CenterVertically)
+        )
+        IconButton(
+            modifier = modifier.scale(1.6F),
+            onClick = { /*TODO*/ }
+        ) {
+            Icon(
+                Icons.Outlined.AccountCircle,
+                contentDescription = null,
+                tint = MaterialTheme.colorScheme.secondary,
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun PracticesListLayout(modifier: Modifier = Modifier) {
+    Surface(
+        modifier = modifier,
+        color = MaterialTheme.colorScheme.background
+    ) {
+        Column(modifier = modifier)
+        {
+            MainHeader()
+            val numbers = (0..3).toList()
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                verticalArrangement = Arrangement.Top,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                userScrollEnabled = true,
+                modifier = modifier
+            ) {
+                items(numbers.size) {
+                    PracticeCard(
+                        practiceNum = it,
+                        modifier = modifier
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PracticesListLayoutPreview() {
+    BreatheTheme {
+        PracticesListLayout(Modifier.fillMaxSize())
+    }
+}
