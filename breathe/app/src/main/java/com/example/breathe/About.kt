a0670bbc82658bc5157d420842fcc76805eee7bf
Alekseev Roman <alexeevroma2002@mail.ru>
2024-03-05 21:00:35 +0300

    remove class

--- breathe/app/src/main/java/com/example/breathe/About.kt
+++ breathe/app/src/main/java/com/example/breathe/About.kt
@@ -1,8 +1,5 @@
 package com.example.breathe
 
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -30,17 +27,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.breathe.ui.theme.BreatheTheme
-
-class About : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContent {
-            BreatheTheme {
-                AboutLayout(Modifier.fillMaxSize())
-            }
-        }
-    }
-}
 
 @Composable
 fun AboutHeader(modifier: Modifier = Modifier) {
