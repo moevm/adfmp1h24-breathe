7f321589d89bfc982686cb7935547b68463ccc94
govoruyshchenkoanastasia <orhideya577@gmail.com>
2024-03-17 12:59:44 +0300

    make accelerometer without activity

---
+++ breathe/app/src/main/java/com/example/breathe/AccelerometerHandler.kt
@@ -0,0 +1,39 @@
+package com.example.breathe
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.VibrationEffect
+import android.os.Vibrator
+
+class AccelerometerHandler(ctx: Context) {
+    private val sManager = ctx.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val vibrator = ctx.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+    var data: Float = 0.0F
+        private set
+
+    fun register() {
+        val sensor = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        val sListener = object : SensorEventListener {
+            override fun onSensorChanged(sEvent: SensorEvent?) {
+                val value = sEvent?.values  // Ускорения по осям XYZ
+                if (value != null) {
+                    data = value[2]       // Ось Z
+                }
+            }
+
+            override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+        }
+        sManager.registerListener(sListener, sensor, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+    fun vibrate() {
+        val effect = VibrationEffect.createOneShot(
+            500, VibrationEffect.DEFAULT_AMPLITUDE
+        )
+        vibrator.vibrate(effect)
+    }
+}
